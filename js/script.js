d3f0f80d9812437d3e72e0a99ca77bce71563d4e
Nithesh S Kotian <nknitheshkotian23@gmail.com>
2023-11-10 09:30:47 +0530

    canvas circuit diagram

--- js/script.js
+++ js/script.js
@@ -2,38 +2,318 @@ var v1 = v2 = r1 = r2 = rf = v0 = 0;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-function resistance2Display(res){
-  let text = `${res} Ω`;
-  ctx.fillStyle = "white";
-  ctx.fillRect(715,300,100,25);
-  ctx.fillStyle = "black";
-  ctx.font = "small-caps 20px Arial";
-  ctx.textBaseline = "middle";
-  ctx.fillText(text, 730, 310)
-  ctx.fillStyle = "black";
-}
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
 
-function resistance1Display(res){
-  let text = `${res} Ω`;
-  ctx.fillStyle = "white";
-  ctx.fillRect(155,90,100,25);
-  ctx.fillStyle = "black";
-  ctx.font = "small-caps 20px Arial";
-  ctx.textBaseline = "middle";
-  ctx.fillText(text, 158, 100)
-  ctx.fillStyle = "black";
-}
+//horizontal
+ctx.beginPath();
+ctx.moveTo(250, 200);
+ctx.lineTo(430, 200);
+ctx.stroke();
 
-function resistancefDisplay(res){
-  let text = `${res} Ω`;
-  ctx.fillStyle = "white";
-  ctx.fillRect(415,300,100,25);
-  ctx.fillStyle = "black";
-  ctx.font = "small-caps 20px Arial";
-  ctx.textBaseline = "middle";
-  ctx.fillText(text, 418, 310);
-  ctx.fillStyle = "black";
-}
+ctx.beginPath();
+ctx.moveTo(540, 235);
+ctx.lineTo(600, 235);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(250, 270);
+ctx.lineTo(430, 270);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(490, 100);
+ctx.lineTo(600, 100);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(420, 100);
+ctx.lineTo(360, 100);
+ctx.stroke();
+
+ctx.beginPath();  
+ctx.moveTo(140, 270);
+ctx.lineTo(180, 270);
+ctx.stroke();
+
+ctx.beginPath();  
+ctx.moveTo(140, 200);
+ctx.lineTo(180, 200);
+ctx.stroke();
+
+
+//opamp
+ctx.beginPath();
+ctx.moveTo(540, 235);
+ctx.lineTo(430, 295);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(540, 235);
+ctx.lineTo(430, 175);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(430, 175);
+ctx.lineTo(430, 295);
+ctx.stroke();
+
+//vertical
+
+ctx.beginPath();
+ctx.moveTo(360, 100);
+ctx.lineTo(360, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(600, 100);
+ctx.lineTo(600, 235);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(369, 270);
+ctx.lineTo(369, 309);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(369, 385);
+ctx.lineTo(369, 425);
+ctx.stroke();
+
+//resistor1
+
+ctx.beginPath();
+ctx.moveTo(180, 201);
+ctx.lineTo(190,188);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(190, 188);
+ctx.lineTo(200,212);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(200, 212);
+ctx.lineTo(210,188);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(210, 188);
+ctx.lineTo(220,212);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(220, 212);
+ctx.lineTo(230,188);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(230, 188);
+ctx.lineTo(240,212);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(240, 212);
+ctx.lineTo(250,200);
+ctx.stroke();
+
+//resistor2
+
+ctx.beginPath();
+ctx.moveTo(180, 271);
+ctx.lineTo(190,258);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(190, 258);
+ctx.lineTo(200,282);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(200, 282);
+ctx.lineTo(210,258);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(210, 258);
+ctx.lineTo(220,282);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(220, 282);
+ctx.lineTo(230,258);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(230, 258);
+ctx.lineTo(240,282);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(240, 282);
+ctx.lineTo(250,270);
+ctx.stroke();
+
+//resistor3
+
+ctx.beginPath();
+ctx.moveTo(420, 101);
+ctx.lineTo(430,88);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(430, 88);
+ctx.lineTo(440,112);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(440, 112);
+ctx.lineTo(450,88);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(450, 88);
+ctx.lineTo(460,112);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(460, 112);
+ctx.lineTo(470,88);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(470, 88);
+ctx.lineTo(480,112);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(480, 112);
+ctx.lineTo(490,100);
+ctx.stroke();
+
+//resistor4
+      
+ctx.beginPath();
+ctx.moveTo(360, 380);
+ctx.lineTo(370, 386);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(360, 380);
+ctx.lineTo(380, 368);
+ctx.stroke();
+      
+ctx.beginPath();
+ctx.moveTo(380, 368);
+ctx.lineTo(360, 356);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(360, 356);
+ctx.lineTo(380, 342);
+ctx.stroke();
+      
+ctx.beginPath();
+ctx.moveTo(380, 342);
+ctx.lineTo(360, 330);
+ctx.stroke();
+      
+ctx.beginPath();
+ctx.moveTo(360, 330);
+ctx.lineTo(380, 318);
+ctx.stroke();
+      
+ctx.beginPath();
+ctx.moveTo(380, 318);
+ctx.lineTo(368, 308);
+ctx.stroke();
+
+//ground
+ctx.beginPath();  
+ctx.moveTo(385, 450);
+ctx.lineTo(355, 450);
+ctx.stroke();
+      
+ctx.beginPath();
+ctx.moveTo(380, 455);
+ctx.lineTo(360, 455);
+ctx.stroke();
+      
+ctx.beginPath();
+ctx.moveTo(375, 460);
+ctx.lineTo(365, 460);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(369, 425);
+ctx.lineTo(369, 450);
+ctx.stroke();
+
+// resistor 1 naming
+ctx.font = "bold small-caps 20px Arial";
+ctx.textBaseline = "middle";
+ctx.fillText("r1", 185, 175)
+
+// resistor 2 naming
+ctx.font = "bold small-caps 20px Arial";
+ctx.textBaseline = "middle";
+ctx.fillText("r2", 185, 300)
+
+// resistor 3 naming
+ctx.font = "bold small-caps 20px Arial";
+ctx.textBaseline = "middle";
+ctx.fillText("Rf", 435, 75)
+
+// resistor 4 naming
+ctx.font = "bold small-caps 20px Arial";
+ctx.textBaseline = "middle";
+ctx.fillText("R3", 400, 345)
+
+//positive naming
+ctx.font = "bold small-caps 20px Arial";
+ctx.textBaseline = "middle";
+ctx.fillText("+", 435, 200)
+
+//negative naming
+ctx.font = "bold small-caps 20px Arial";
+ctx.textBaseline = "middle";
+ctx.fillText("_", 440, 260)
+
+ctx.fillStyle = "black";
+ctx.fillRect(200,200, 1, 1);
+
+// function resistance2Display(res){
+//   let text = `${res} Ω`;
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(715,300,100,25);
+//   ctx.fillStyle = "black";
+//   ctx.font = "small-caps 20px Arial";
+//   ctx.textBaseline = "middle";
+//   ctx.fillText(text, 730, 310)
+//   ctx.fillStyle = "black";
+// }
+
+// function resistance1Display(res){
+//   let text = `${res} Ω`;
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(155,90,100,25);
+//   ctx.fillStyle = "black";
+//   ctx.font = "small-caps 20px Arial";
+//   ctx.textBaseline = "middle";
+//   ctx.fillText(text, 158, 100)
+//   ctx.fillStyle = "black";
+// }
+
+// function resistancefDisplay(res){
+//   let text = `${res} Ω`;
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(415,300,100,25);
+//   ctx.fillStyle = "black";
+//   ctx.font = "small-caps 20px Arial";
+//   ctx.textBaseline = "middle";
+//   ctx.fillText(text, 418, 310);
+//   ctx.fillStyle = "black";
+// }
 
 //Initialise system parameters here
 function varinit() {
@@ -181,9 +461,9 @@ function varupdate() {
   res2 = $("#resistor2Spinner").spinner("value");
   resf = $("#resistorfSpinner").spinner("value");
 
-  resistance2Display(res2);
-  resistance1Display(res1);
-  resistancefDisplay(resf);
+  // resistance2Display(res2);
+  // resistance1Display(res1);
+  // resistancefDisplay(resf);
 
   v1Assign(volt1);
   v2Assign(volt2);
